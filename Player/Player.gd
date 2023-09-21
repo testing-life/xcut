75cb9f43b73d74b0fa9b6e1f6aa56df606c98b19
M <aethelbrihte@gmail.com>
2023-09-21 20:13:15 +0200

    movement gravity

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,8 +3,8 @@ extends CharacterBody2D
 signal hit
 
 @export var walk_speed = 400
-@export var jump_speed = -400
-@export var gravity = 4000
+@export var jump_speed = -1000
+@export var gravity = 2000
 
 var is_attacking = false
 var screen_size
@@ -13,8 +13,8 @@ func _ready():
 	screen_size = get_viewport_rect()
 
 func _physics_process(delta):
-	#velocity.y += gravity + delta
+	velocity.y += gravity * delta
 	velocity.x = Input.get_axis("m_left","m_right") * walk_speed
 	move_and_slide()
-	if Input.is_action_just_pressed('m_jump') and is_on_floor(): 
-		velocity.y += jump_speed
+	if Input.is_action_pressed('m_jump') and is_on_floor(): 
+		velocity.y = jump_speed
