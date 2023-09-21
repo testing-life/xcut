f915c94357d8036e2757bcf5c5fe579c349e6e0d
M <aethelbrihte@gmail.com>
2023-09-21 18:38:55 +0200

    move with gravity

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,34 +4,17 @@ signal hit
 
 @export var walk_speed = 400
 @export var jump_speed = -400
+@export var gravity = 4000
 
+var is_attacking = false
 var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("m_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("m_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("m_jump") && is_on_floor():		
-		velocity.y = jump_speed
-		
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * walk_speed
-		$AnimatedSprite2D.play()
-	else:
-		$AnimatedSprite2D.stop()
-	position += velocity * delta
-	# poition = position.clamp(Vector2.ZERO, screen_size)
-
-	if velocity.x != 0:
-		$AnimatedSprite2D.animation = 'run'
-		$AnimatedSprite2D.flip_v = false
-		$AnimatedSprite2D.flip_h = velocity.x < 0
-	elif velocity.y != 0:
-		$AnimatedSprite2D.flip_v = velocity.y > 0
+func _physics_process(delta):
+	#velocity.y += gravity + delta
+	velocity.x = Input.get_axis("m_left","m_right") * walk_speed
+	move_and_slide()
+	if Input.is_action_just_pressed('m_jump') and is_on_floor(): 
+		velocity.y += jump_speed
