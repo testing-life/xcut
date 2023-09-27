10fb801f3f6488adda8ad2d683b0ddb2311ad56a
M <aethelbrihte@gmail.com>
2023-09-27 08:47:17 +0200

    walk/cut

--- Player/Player.gd
+++ Player/Player.gd
@@ -15,6 +15,14 @@ func _ready():
 func _physics_process(delta):
 	velocity.y += gravity * delta
 	velocity.x = Input.get_axis("m_left","m_right") * walk_speed
+	print(velocity.x)
 	move_and_slide()
+	if velocity.x != 0:
+		$AnimatedSprite2D.play("run")
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+		
 	if Input.is_action_pressed('m_jump') and is_on_floor(): 
 		velocity.y = jump_speed
+		
+	if Input.is_action_just_pressed("f_cut"):
+		$AnimatedSprite2D.play("fight")
