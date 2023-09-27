cec9c901f54c1a22663b66ad3a4f04ddb5e653cc
M <aethelbrihte@gmail.com>
2023-09-27 13:14:29 +0200

    attacking

--- Player/Player.gd
+++ Player/Player.gd
@@ -10,6 +10,7 @@ signal hit
 @onready var sprite = $Anim
 var is_attacking = false
 var screen_size
+var attacks = ['attack1','attack2']
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect()
@@ -23,18 +24,30 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed('m_jump') and is_on_floor():
 		velocity.y = jump_speed 
+	
+	if Input.is_action_just_pressed('f_cut'):
+		is_attacking = true
 		
 	move_and_slide()
 	update_animation(horizontal_dir)
 
 func update_animation(horizontal_direction):
-	if is_on_floor():
-		if horizontal_direction == 0:
-			ap.play("idle")
+	if is_attacking == false:
+		if is_on_floor():
+			if horizontal_direction == 0:
+				ap.play("idle")
+			else:
+				ap.play("walk_right")
 		else:
-			ap.play("walk_right")
+			if velocity.y < 0:
+				ap.play("jump")
+			else:
+				ap.play("fall")
 	else:
-		if velocity.y < 0:
-			ap.play("jump")
-		else:
-			ap.play("fall")
+		ap.play('attack1')
+
+
+func _on_animation_player_animation_finished(anim_name):
+	print(anim_name)
+	if anim_name == 'attack1':
+		is_attacking = false # Replace with function body.
