b8f1a91eba52100d8e64a84b4e363066e54ef299
M <aethelbrihte@gmail.com>
2023-10-04 12:32:13 +0200

    tilemap, light

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -16,8 +16,8 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	#w	velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
