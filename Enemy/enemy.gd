6d513ab1eecc7ded0ec14dff1368bf8665c99bc7
M <aethelbrihte@gmail.com>
2023-10-09 19:20:55 +0200

    movement

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,36 +1,46 @@
 extends CharacterBody2D
 
 
-@export var SPEED = 300.0
-@export var JUMP_VELOCITY = -400.0
+@export var speed = 150.0
 @export var health = 100
 @onready var sprite = $Sprite2D
 @onready var ap = $AnimationPlayer
+@onready var cast = $RayCast2D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var facing_right = true;
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle Jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	#w	velocity.y = JUMP_VELOCITY
-
+	if !cast.is_colliding() && is_on_floor():
+		flip()
+			
+	velocity.x = speed
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-		sprite.flip_h = direction == -1
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	#var direction = Input.get_axis("ui_left", "ui_right")
+	#if direction:
+	#	velocity.x = direction * SPEED
+	#	sprite.flip_h = direction == -1
+	#else:
+	#	velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-	updateAnimation(direction)
+	updateAnimation(velocity.x)
 	
+func flip():
+	facing_right = !facing_right
+	print(abs(velocity.x))
+	sprite.flip_h = !facing_right
+	if facing_right:
+		speed = abs(speed) 
+		$RayCast2D.position.x = 22
+	else:
+		$RayCast2D.position.x = -22
+		speed = abs(speed)  * -1
+			
 func updateAnimation(horizontal_direction):
 	if horizontal_direction == 0:
 		ap.stop()
